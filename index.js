997556396ba5e0c08c691e4e7458967a9772f52d
ninazmul <nazmulsaw@gmail.com>
2024-01-06 19:28:47 +0600

    loading user, post get user and delete or make user admin done

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+// const itemCollection = client.db("portfolioDB").collection("portfolio");
 
+// app.get("/items", async (req, res) => {
+//   const result = await itemCollection.find().toArray();
+//   res.send(result);
+// });
 
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8yqorq.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,7 +30,78 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const itemCollection = client.db("portfolioDB").collection("portfolio");
+
+    const userCollection = client.db("portfolioDB").collection("users");
+
+    // users api 
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch(
+      "/users/admin/:id",
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+    // app.get("/users/admin/:email", verifyToken, async (req, res) => {
+    //   const email = req.params.email;
+
+    //   if (email !== req.decoded.email) {
+    //     return res.status(403).send({ message: "forbidden access" });
+    //   }
+
+    //   const query = { email: email };
+    //   const user = await userCollection.findOne(query);
+    //   let admin = false;
+    //   if (user) {
+    //     admin = user ? user.role === "admin" : false;
+    //   }
+    //   res.send({ admin });
+    // });
+
+    app.get('/items', async (req, res) => {
+      const result = await itemCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/items', async (req, res) => {
+      const item = req.body;
+      const result = await itemCollection.insertOne(item);
+      res.send(result);
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,17 +109,14 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-app.get('/', (req, res) => {
-    res.send('here is my PORTFOLIO')
+app.get("/", (req, res) => {
+  res.send("N.I. Nazmul's portfolio server is running...");
 });
 
 app.listen(port, () => {
-    console.log(`my PORTFOLIO is running on port ${port}`)
+  console.log(`N.I. Nazmul's portfolio server is running on port ${port}`);
 });
